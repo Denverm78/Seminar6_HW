51526dbbf013def1cccd2ca57f2337121b6df98a
Denis <denverm@yandex.ru>
2022-10-16 16:24:45 +0300

    Changed Task1

--- Task1.py
+++ Task1.py
@@ -2,7 +2,11 @@
 # Примеры:
 # - 5 -> -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
 
+# number_n = int(input("Введите число N: "))
+# for i in range(-number_n, number_n):
+#     print(i, end=', ')
+# print(number_n)
+
 number_n = int(input("Введите число N: "))
-for i in range(-number_n, number_n):
-    print(i, end=', ')
-print(number_n)
+list = [i for i in range(-number_n, number_n+1)]
+print(", ".join(str(x) for x in list))
