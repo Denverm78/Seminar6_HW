8684d00b230abfb0ebce96b731ec6dae790f5253
Denis <denverm@yandex.ru>
2022-10-16 16:26:48 +0300

    Changed Task11

--- Task11.py
+++ Task11.py
@@ -1,13 +1,24 @@
 # Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
 
+# numberN = int(input("Введите число N: "))
+# if numberN <= 0:
+#     print("Вы ввели неверное число")
+#     exit()
+# result = []
+# sum = 0
+# for i in range(1, numberN+1):
+#     elementN = round((1+1/i)**i, 3)
+#     sum += elementN
+#     result.append(elementN)
+# print(f"Сумма {numberN} чисел последовательности (1+1/N)^N: {result} равна {sum}")
+
 numberN = int(input("Введите число N: "))
 if numberN <= 0:
     print("Вы ввели неверное число")
     exit()
-result = []
+list_element = list(range(1,numberN+1))
+result = list(map(lambda x: round((1+1/x)**x, 3), list_element))
 sum = 0
-for i in range(1, numberN+1):
-    elementN = round((1+1/i)**i, 3)
-    sum += elementN
-    result.append(elementN)
+for i in result:
+    sum +=i
 print(f"Сумма {numberN} чисел последовательности (1+1/N)^N: {result} равна {sum}")
